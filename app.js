693ca7dc78241fedd678fa85c4fcee362f16466e
consihon <connorsihon@gmail.com>
2018-09-21 20:17:35 -0700

    problem 2 solved

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
-Write a function called sum() that takes in two numbers as arguments and then returns an array where the 
+Write a function called sum() that takes in two numbers as arguments and then returns an array where the
 first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows
  this example and uses the values that were input into the function:
 
@@ -12,7 +12,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    var c= a+b;
+  var c= a+b;
   return([c,'The sum of '+a+' and '+b+' is '+c+'.']);
 }
 
@@ -23,7 +23,9 @@ testSum(4, 7);
 
 /////////////////////////////////////
 /* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiply() that takes in two numbers as arguments and returns
+an array where the first element is the product of those numbers, and the second element
+ is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The product of 5 and 9 is 45."
 
@@ -31,11 +33,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var c= (a*b);
+  return([c,'The product of '+a+' and '+b+' is '+c+'.']);
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
